6b4fa9c5a88f81d82d497da7baf636753fc44499
Ayush Bansal <ayushb268@gmail.com>
2022-01-19 00:22:08 +0530

    Add RequestRealTimeGraph query functionality

--- scala_core/src/main/scala/org/nimbleedge/recoedge/models/TopologyTree.scala
+++ scala_core/src/main/scala/org/nimbleedge/recoedge/models/TopologyTree.scala
@@ -8,13 +8,16 @@ sealed abstract class TopologyTree {
 	val digest : Array[Byte] = computeDigest()
 	override val hashCode : Int = ByteBuffer.wrap(digest.slice(0,4)).getInt
 
-	def hash(baseName: String, args: List[TopologyTree]): Array[Byte] = {
+	def hash(baseName: String, args: Set[TopologyTree]): Array[Byte] = {
 		val md = MD.getInstance("SHA-256")
 		md.reset()
 		md.update(baseName.getBytes("UTF-8"))
-		args.foreach(
-			arg => md.update(arg.digest)
-		)
+
+		// Two Same Sets will always have the same hashcode
+		// Instead of computing hashcode separately for the sets,
+		// We use their internal one.
+		md.update(ByteBuffer.allocate(4).putInt(args.hashCode()).array())
+
 		md.digest()
 	}
 
@@ -26,7 +29,6 @@ sealed abstract class TopologyTree {
 	}
 }
 
-
 // Creating Leaf and Node separately for keeping the distinction between Trainers
 // and Orchestrators + Aggregators
 
@@ -34,12 +36,12 @@ sealed abstract class TopologyTree {
 case class Leaf(value: TrainerIdentifier) extends TopologyTree {
 	override def toString(): String = value.name()
 
-	override def computeDigest(): Array[Byte] = hash("_Leaf" + value.name(), List.empty)
+	override def computeDigest(): Array[Byte] = hash("_Leaf" + value.name(), Set.empty)
 }
 
 // Node can be only OrchestratorIdentifier or AggregatorIdentifier
 // It should have more that one children
-case class Node(value: Either[OrchestratorIdentifier, AggregatorIdentifier], children: List[TopologyTree]) extends TopologyTree {
+case class Node(value: Either[OrchestratorIdentifier, AggregatorIdentifier], children: Set[TopologyTree]) extends TopologyTree {
 	override def toString(): String = {
 		val node_value : String = value match {
 			case Left(x) => x.name()
